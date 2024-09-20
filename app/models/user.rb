ba680a56e1a87eecbe22943419c65111c139d303
marco-duart <marcoaurelio_db@hotmail.com>
2024-09-20 09:19:06 -0300

    chore: formatter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,32 @@
 class User < ApplicationRecord
-devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-belongs_to :team, optional: true
-has_many :managed_teams, class_name: 'Team', foreign_key: 'manager_id', dependent: :nullify
+  belongs_to :team, optional: true
+  has_many :managed_teams, class_name: 'Team', foreign_key: 'manager_id', dependent: :nullify
 
-enum role: { employee: 0, monitor: 1, manager: 2, admin: 3 }
+  enum role: { employee: 0, monitor: 1, manager: 2, admin: 3 }
 
-validates :role, presence: true
+  validates :role, presence: true
 
-before_save :set_default_role, if: :new_record?
+  before_save :set_default_role, if: :new_record?
 
-def set_default_role
-  self.role ||= :employee
-end
+  def set_default_role
+    self.role ||= :employee
+  end
 
-def manager?
-  self.role == "manager"
-end
+  def manager?
+    self.role == 'manager'
+  end
 
-def employee?
-  self.role == "employee"
-end
+  def employee?
+    self.role == 'employee'
+  end
 
-def monitor?
-  self.role == "monitor"
-end
+  def monitor?
+    self.role == 'monitor'
+  end
 
-def admin?
-  self.role == "admin"
-end
+  def admin?
+    self.role == 'admin'
+  end
 end
