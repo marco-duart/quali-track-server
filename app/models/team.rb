ee8ac73a32cf629a61f78d97bd783488c35bf3ba
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-02-19 08:45:53 -0300

    feat: updated teams relations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   has_many :users, -> { where(role: :employee) }, class_name: 'User', dependent: :nullify
   belongs_to :manager, class_name: 'User', foreign_key: 'manager_id'
+  belongs_to :department
 
   validates :name, presence: true
   validates :manager_id, presence: true
